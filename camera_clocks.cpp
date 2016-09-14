074c3009484c59905e024dafb92c08d1a7cb5093
kushgpt23 <kushgpt23@gmail.com>
2016-09-14 11:16:50 -0700

    Add files via upload

---
+++ camera_clocks.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+
+#include "okFrontPanelDLL.h"
+#include "time.h"
+
+
+#define XILINX_CONFIGURATION_FILE   "camera_clocks_v1.bit"
+#define ALTERA_CONFIGURATION_FILE   "des.rbf"
+#if defined(_WIN32)
+#define strncpy strncpy_s
+#define sscanf  sscanf_s
+#endif
+
+bool
+clock_generation(okCFrontPanel *xem, char *outfilename)
+{
+	int i;
+	std::ofstream f_out;
+	unsigned long a,b;
+	//time_t current_time;
+	__int64 initial_time,current_time,frequency;
+	double time;
+	// Assert then deassert RESET.
+	xem->SetWireInValue(0x00, 0xff, 0x01);
+	xem->UpdateWireIns();
+	xem->SetWireInValue(0x00, 0x00, 0x01);
+	xem->UpdateWireIns();
+
+
+	// Open the output file.
+	f_out.open(outfilename, std::ios::binary | std::ofstream::app);
+	if (false == f_out.is_open()) {
+		printf("Error: Output file could not be opened.\n");
+		return(false);
+	}
+	QueryPerformanceFrequency((LARGE_INTEGER *)&frequency);
+	QueryPerformanceCounter((LARGE_INTEGER *)&initial_time);
+	unsigned long start_time, new_time, time2;
+	start_time = timeGetTime();
+	for (i = 0; i < 1000; i++) {
+		xem->UpdateWireOuts();
+		QueryPerformanceCounter((LARGE_INTEGER *) &current_time);
+		new_time = timeGetTime();
+		//current_time = time(NULL);
+		a = xem->GetWireOutValue(0x20);
+		b = xem->GetWireOutValue(0x21);
+		//f_out << a[0] << "\t" << a[4]<<a[3]<<a[2]<<a[1]<<"\n";
+		time = ((current_time-initial_time)*1.0 / frequency)*1000000.0;
+		time2 = new_time - start_time;
+		f_out << a<<"\t"<<b<<"\t"<< time<<"\t"<<time2<<"\r\n";
+		}
+	f_out.close();
+
+	return(true);
+}
+
+
+okCFrontPanel *
+initializeFPGA()
+{
+	okCFrontPanel *dev;
+	okCPLL22393 pll22393;
+	okCFrontPanel::ErrorCode error;
+	std::string   config_filename;
+
+	// Open the first XEM - try all board types.
+	dev = new okCFrontPanel;
+	if (okCFrontPanel::NoError != dev->OpenBySerial()) {
+		delete dev;
+		printf("Device could not be opened.  Is one connected?\n");
+		return(NULL);
+	}
+
+	printf("Found a device: %s\n", dev->GetBoardModelString(dev->GetBoardModel()).c_str());
+	
+	error = dev->GetPLL22393Configuration(pll22393);
+	pll22393.SetReference(48.0f);//48  //1.0  //48
+	pll22393.SetPLLParameters(0, 100, 48);//0,10,48 //0,6,192  //0,100,48
+	pll22393.SetOutputSource(0, okCPLL22393::ClkSrc_PLL0_0);
+	pll22393.SetOutputDivider(0, 1); //100
+	pll22393.SetOutputEnable(0, true);
+	//pll22393.SetPLLLF(0, 100);
+	dev->SetPLL22393Configuration(pll22393);
+	double freq = pll22393.GetPLLFrequency(0);
+	double freq2 = pll22393.GetOutputFrequency(0);
+	std::cout<< "clock frequency is "<<freq<<"\n";
+	std::cout << "Output frequency is " << freq2 << "\n";
+	//dev->LoadDefaultPLLConfiguration();
+
+	// Get some general information about the XEM.
+	std::string str;
+	printf("Device firmware version: %d.%d\n", dev->GetDeviceMajorVersion(), dev->GetDeviceMinorVersion());
+	str = dev->GetSerialNumber();
+	printf("Device serial number: %s\n", str.c_str());
+	str = dev->GetDeviceID();
+	printf("Device device ID: %s\n", str.c_str());
+
+	// Download the configuration file.
+	switch (dev->GetBoardModel()) {
+	case okCFrontPanel::brdZEM4310:
+	case okCFrontPanel::brdZEM5305A2:
+		config_filename = ALTERA_CONFIGURATION_FILE;
+		break;
+	default:
+		config_filename = XILINX_CONFIGURATION_FILE;
+		break;
+	}
+
+	if (okCFrontPanel::NoError != dev->ConfigureFPGA(config_filename)) {
+		printf("FPGA configuration failed.\n");
+		delete dev;
+		return(NULL);
+	}
+
+	// Check for FrontPanel support in the FPGA configuration.
+	if (dev->IsFrontPanelEnabled())
+		printf("FrontPanel support is enabled.\n");
+	else
+		printf("FrontPanel support is not enabled.\n");
+
+	return(dev);
+}
+
+
+static void
+printUsage(char *progname)
+{
+	printf("Usage: %s outfile\n", progname);
+	printf("   outfile - destination output file.\n");
+}
+
+
+int
+main(int argc, char *argv[])
+{
+	char outfilename[128];
+	char dll_date[32], dll_time[32];
+	
+	printf("---- Opal Kelly ---- FPGA-DES Application v1.0 ----\n");
+	if (FALSE == okFrontPanelDLL_LoadLib(NULL)) {
+		printf("FrontPanel DLL could not be loaded.\n");
+		return(-1);
+	}
+	okFrontPanelDLL_GetVersion(dll_date, dll_time);
+	printf("FrontPanel DLL loaded.  Built: %s  %s\n", dll_date, dll_time);
+
+
+	if (argc != 2) {
+		printUsage(argv[0]);
+		return(-1);
+	}
+
+	strncpy(outfilename, argv[1], 128);
+	
+	// Initialize the FPGA with our configuration bitfile.
+	okCFrontPanel *xem;
+	xem = initializeFPGA();
+	if (NULL == xem) {
+		printf("FPGA could not be initialized.\n");
+		return(-1);
+	}
+
+	// Now perform the encryption/decryption process.
+	if (clock_generation(xem, outfilename) == false) {
+		printf("Clock generation process failed.\n");
+		return(-1);
+	}
+	else {
+		printf("Clock generation process succeeded!\n");
+	}
+
+	return(0);
+}
---
+++ camera_clocks_v1.v
@@ -0,0 +1,111 @@
+module camera_clocks_v1(input clk,
+	input  wire [7:0]  hi_in,
+	output wire [1:0]  hi_out,
+	inout  wire [15:0] hi_inout,
+	inout  wire        hi_aa,
+
+	output wire        i2c_sda,
+	output wire        i2c_scl,
+	output wire        hi_muxsel,
+	output wire			 Vrst_sel0,
+	output wire        rst,
+	output wire			 sample,
+	output wire			 precharge,
+	output wire			 mem_sel,
+	output wire			 pulse,
+	output wire			 row_select
+	);
+	
+//initialization
+reg [13:0] counter1,counter;
+wire reset;
+wire rst_sample;
+
+//Target interface bus:
+wire        ti_clk;
+wire [30:0] ok1;
+wire [16:0] ok2;
+
+// Endpoint connections:
+wire [15:0]  ep00wire;
+wire [15:0]  ep01wire;
+wire [15:0]  ep02wire;
+wire [15:0]  ep20wire;
+wire [15:0]  ep21wire;
+wire [15:0] TrigIn40;
+
+assign i2c_sda = 1'bz;
+assign i2c_scl = 1'bz;
+assign hi_muxsel = 1'b0;
+
+// Instantiate the okHost and connect endpoints.
+wire [17*2-1:0]  ok2x;
+okHost okHI(
+	.hi_in(hi_in), .hi_out(hi_out), .hi_inout(hi_inout), .hi_aa(hi_aa), .ti_clk(ti_clk),
+	.ok1(ok1), .ok2(ok2));
+
+okWireOR # (.N(2)) wireOR (.ok2(ok2), .ok2s(ok2x));
+
+okWireIn     ep00 (.ok1(ok1),                          .ep_addr(8'h00), .ep_dataout(ep00wire));
+//okWireIn     ep01 (.ok1(ok1),                          .ep_addr(8'h01), .ep_dataout(ep01wire));
+//okWireIn     ep02 (.ok1(ok1),                          .ep_addr(8'h02), .ep_dataout(ep02wire));
+//okTriggerIn  ep40 (.ok1(ok1),                           .ep_addr(8'h40), .ep_clk(clk), .ep_trigger(TrigIn40));
+
+okWireOut    ep20 (.ok1(ok1), .ok2(ok2x[ 0*17 +: 17 ]), .ep_addr(8'h20), .ep_datain(ep20wire));
+okWireOut    ep21 (.ok1(ok1), .ok2(ok2x[ 1*17 +: 17 ]), .ep_addr(8'h21), .ep_datain(ep21wire));
+
+//Signals to and fro host
+assign reset = ep00wire[0];
+assign ep20wire = {15'b0,sample};
+assign ep21wire = {15'b0, rst};
+
+
+//Output pin assignment using counter values
+
+wire [9:0] delay1 = 11;				// multiples of 10ns
+wire [9:0] delay = 10;				// multiples of 10ns
+wire [9:0] exposure_time = 100;	// multiples of 10ns
+
+
+assign Vrst_sel0 =
+		(1<=counter1 && counter1<=delay1) ? 1'b0 : 1'b1;		
+assign rst = 
+		(2<=counter1 && counter1<=(delay1-1)) ? 1'b1 : 1'b0;
+
+
+negedge_detecter i1 (.A(rst), .rst(reset), .clk(clk), .delay(delay), .Y(rst_sample));
+negedge_detecter i2 (.A(rst_sample), .rst(reset), .clk(clk), .delay(exposure_time-delay), .Y(precharge));
+negedge_detecter i3 (.A(rst_sample), .rst(reset), .clk(clk), .delay(exposure_time+10'b1010-delay), .Y(sample));
+
+assign mem_sel =
+		(14'b1<=counter1 && counter1<=(delay1+exposure_time+15)) ? 1'b1: 1'b0;
+
+assign pulse =		
+		(14'b1<=counter1 && counter1<=(delay1+exposure_time+16)) ? 1'b0: 1'b1;
+
+assign row_select =
+		((delay1+exposure_time+18)<=counter1 && counter1<=(delay1+exposure_time+401)) ? 1'b1: 1'b0;		
+
+//Main Counter		
+always @(posedge clk)
+begin
+	if (reset)
+		counter<=14'b0;
+	else
+	begin
+		if (counter == 14'b10011100001111)
+			counter<=14'b0;
+		else counter<=counter+14'b1;
+	end
+end
+
+//Flip flop to latch out 
+always @(clk or counter)
+begin
+	if (reset)
+	counter1<=14'b0;
+	else if (~clk)
+	counter1<=counter;
+end
+
+endmodule
