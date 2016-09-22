e400aa88e701fbc3bd34ed3aa18b6c1ba9534e0a
Max <maxfooo@gmail.com>
2016-09-22 16:01:57 -0700

    Create MessageTexts.py

---
+++ MessageTexts.py
@@ -0,0 +1,26 @@
+'''
+Created on Sep 19, 2016
+
+@author: Alphacore Engineer 1
+'''
+
+
+data_file_error_text = """
+Something went wrong with the file you tried to open or load. Make sure the file name, if given manually, has the extension as well as the name. 
+"""
+
+configure_FPGA_error_text = """
+Could not configure FPGA. 
+"""
+
+connect_to_device_error_text = """
+Device could not be opened. Is one connected? 
+"""
+
+FPGA_Communication_repr_text = """
+Device firmware version: {0}.{1}
+Device serial number: {2}
+Device ID: {3}
+Board model: {4}
+FrontPanel Enabled?: {5}
+"""
