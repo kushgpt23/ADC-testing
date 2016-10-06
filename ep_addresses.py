769158fd8b0dedb6bb0f7442cbb131c8bd568b01
Max <maxfooo@gmail.com>
2016-10-06 14:05:28 -0700

    Create ep_addresses.py

---
+++ ep_addresses.py
@@ -0,0 +1,10 @@
+'''
+Created on Oct 6, 2016
+
+@author: Max Ruiz
+'''
+ADC_DATA_ADDR = 0xA0
+FIFO_DATA_COUNT_ADDR = 0xA1
+FIFO_EMPTY_ADDR = 0x20 # bit [0]
+DEBUG_ADDR = 0x20 # bit [1]
+RESET_ADDR = 0x00 # bit [0]
